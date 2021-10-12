65c00fd2bf89500cdddf2bbb81eeb2d707c4b6ef
Danny Willems <be.danny.willems@gmail.com>
2021-10-12 11:40:05 +0200

    Add tests for Utils.bitreverse

--- test/test_utils.ml
+++ test/test_utils.ml
@@ -1,5 +1,31 @@
 let () = Random.self_init ()
 
+let test_bitreverse () =
+  let test_vectors =
+    [ (0, 0);
+      (1, 8);
+      (2, 4);
+      (3, 12);
+      (4, 2);
+      (5, 10);
+      (6, 6);
+      (7, 14);
+      (8, 1);
+      (9, 9);
+      (10, 5);
+      (11, 13);
+      (12, 3);
+      (13, 11);
+      (14, 7);
+      (15, 15) ]
+  in
+  let logn = 4 in
+  List.iter
+    (fun (l, expected_output) ->
+      let output = Utils.bitreverse l logn in
+      assert (output = expected_output))
+    test_vectors
+
 let test_next_power_of_two () =
   let vectors = [(2, 2); (3, 4); (7, 8); (12, 16)] in
   List.iter
@@ -18,4 +44,5 @@ let () =
   run
     "Utils"
     [ ( "Reorganizing the coefficients",
-        [test_case "Next power of two" `Quick test_next_power_of_two] ) ]
+        [ test_case "Next power of two" `Quick test_next_power_of_two;
+          test_case "bitreverse unit test" `Quick test_bitreverse ] ) ]
